c05b6816d2756244a3f8b8d3b73a76fee4727870
yilfri <yilfri97@hotmail.com>
2021-06-29 11:25:06 -0400

    About page added

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Layout = (props) => {
-	return <main>{props.children}</main>;
+	return (
+		<>
+			<h1>Header</h1>
+			<nav>
+				<Link href="/">Home</Link>
+				<Link href="/about">About Us</Link>
+			</nav>
+			<main>{props.children}</main>
+		</>
+	);
 };
 
 export default Layout;
---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/layout/Layout';
+
+const about = () => {
+	return (
+		<div>
+			<Layout>
+				<h1>About Us</h1>
+			</Layout>
+		</div>
+	);
+};
+
+export default about;
