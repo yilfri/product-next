cefddd65fb255c73a2249a22df9a63e0e2008f3c
yilfri <yilfri97@hotmail.com>
2021-06-30 11:39:30 -0400

    Form for Create Account

--- pages/create-account.js
+++ pages/create-account.js
@@ -1,10 +1,31 @@
+import React from 'react';
 import Layout from '../components/layout/Layout';
 
 export default function CreateAccount() {
 	return (
 		<div>
 			<Layout>
-				<h1>Create Account</h1>
+				<>
+					<h1>Create Account</h1>
+					<form>
+						<div>
+							<label htmlFor="name">Name</label>
+							<input type="text" name="name" id="name" placeholder="Your Name" />
+						</div>
+
+						<div>
+							<label htmlFor="email">Email</label>
+							<input type="email" name="email" id="email" placeholder="Your Email" />
+						</div>
+
+						<div>
+							<label htmlFor="password">Password</label>
+							<input type="password" name="password" id="password" placeholder="Your Password" />
+						</div>
+
+						<input type="submit" value="Create Account" />
+					</form>
+				</>
 			</Layout>
 		</div>
 	);
