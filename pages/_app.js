4a280cbc25a9ac9388e28152964e22eaf5807487
yilfri <yilfri97@hotmail.com>
2021-06-30 17:00:19 -0400

    set user when page reload

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import App from 'next/app';
 import firebase, { FirebaseContext } from '../firebase';
 import '../styles/globals.css';
+import useAuth from '../hooks/useAuth';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
+	const user = useAuth();
+	console.log(user);
+
 	return (
 		<FirebaseContext.Provider value={{ firebase }}>
 			<Component {...pageProps} />
