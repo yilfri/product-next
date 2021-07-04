bb70179277a3ccabbc9fa1d85fcea83d7e0ee3a9
yilfri <yilfri97@hotmail.com>
2021-07-04 16:31:12 -0400

    Search successful

--- pages/search.js
+++ pages/search.js
@@ -1,17 +1,42 @@
+import React, { useState, useEffect } from 'react';
 import Layout from '../components/layout/Layout';
 import { useRouter } from 'next/router';
+import ProductDetails from '../components/layout/ProductDetails';
+import useProducts from '../hooks/useProducts';
 
 const Search = () => {
 	const router = useRouter();
 	const {
 		query: { q }
 	} = router;
-	console.log(q);
+
+	// All products.
+	const { products } = useProducts('creation');
+	const [result, setResult] = useState([]);
+
+	useEffect(() => {
+		const search = q.toLowerCase();
+		const filter = products.filter((product) => {
+			return (
+				product.name.toLowerCase().includes(search) ||
+				product.description.toLowerCase().includes(search)
+			);
+		});
+		setResult(filter);
+	}, [q, products]);
 
 	return (
 		<div>
 			<Layout>
-				<h1>Search</h1>
+				<div className="listado-productos">
+					<div className="contenedor">
+						<ul className="bg-white">
+							{result.map((product) => (
+								<ProductDetails key={product.id} product={product} />
+							))}
+						</ul>
+					</div>
+				</div>
 			</Layout>
 		</div>
 	);
